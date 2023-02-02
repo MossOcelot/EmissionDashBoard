25f56a20ef46fe88b59f736159c95f5f465f4f5f
Phumin  Sathipchan <phuminsathipchan@gmail.com>
2023-02-02 15:20:03 +0700

    update dashboard

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, callback, Output, Input
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas
@@ -7,30 +7,25 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 
 colors = {"background": "#111111", "text": "#7FDBFF"}
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
 df = pandas.read_excel("data/ghg-emission-of-thailand-edited-july-2021.xlsx")
-df.columns = ["BM", "emission", "section_groub", "sub_section", "eng_sub_section", "quantity"]
+df.columns = ["BE", "emission", "section_groub", "sub_section", "eng_sub_section", "quantity"]
 
 df['quantity'] = df['quantity'].replace("-",0)
 
 emission_unique = df['emission'].unique()
-years_unique = df['BM'].unique()
+years_unique = df['BE'].unique()
 
-emission_quantity_years = {'BM': [] ,'emission': [], 'quantity': []}
+emission_quantity_years = {'BE': [] ,'emission': [], 'quantity': []}
 for y in years_unique:
-    bm_y_rows = df[df["BM"] == y]
+    bm_y_rows = df[df["BE"] == y]
     for e in emission_unique:
         emission_in_year = bm_y_rows[bm_y_rows["emission"] == e]
         quantity = emission_in_year['quantity'].sum()
-        emission_quantity_years['BM'].append(y)
+        emission_quantity_years['BE'].append(y)
         emission_quantity_years['emission'].append(e)
         emission_quantity_years['quantity'].append(quantity)
 
 df_emission_quantity_years = pandas.DataFrame(emission_quantity_years)
-
-
-
 
 power_emission = df[df['emission'] == 'ภาคพลังงาน']
 # 
@@ -47,10 +42,28 @@ for x in df_emission_quantity_years['emission'].unique():
 
 for v in value:
     df2 = df_emission_quantity_years[(df_emission_quantity_years['emission'].str.contains(v[0])) | ( df_emission_quantity_years['emission'].str.contains(v[1]))]
-    output.append(dcc.Graph(id=f"{v[0]} {v[1]}",figure=px.bar(df2, x="BM", y="quantity", color="emission", orientation="v", barmode="group")))
+    output.append(dcc.Graph(id=f"{v[0]} {v[1]}",figure=px.bar(df2, x="BE", y="quantity", color="emission", orientation="v", barmode="group")))
 
-fig = px.bar(df_emission_quantity_years, x="BM", y="quantity", color="emission", orientation="v", barmode="group")
-fig2 = px.bar(power_emission, x="BM", y="quantity", color="eng_sub_section")
+fig = px.bar(df_emission_quantity_years, x="BE", y="quantity", color="emission", orientation="v", barmode="group")
+fig2 = px.bar(power_emission, x="BE", y="quantity", color="eng_sub_section")
+
+@app.callback(
+    Output("bar-graph", "figure"),
+    Input("emission_type1", "value"),
+    Input("emission_type2", "value"),
+)
+def show_data(emission_type1, emission_type2):
+    print(emission_type1, emission_type2)
+    filtered_df = df_emission_quantity_years[(df_emission_quantity_years['emission'].str.contains(emission_type1)) | 
+    ( df_emission_quantity_years['emission'].str.contains(emission_type2))]
+    # print(filtered_df)
+    fig = px.bar(filtered_df, 
+    x="BE", y="quantity", 
+    color="emission", orientation="v", 
+    barmode="group")
+    # fig.update_layout(transition_duration=500)
+
+    return fig
 
 app.layout = html.Div(
     children=[
@@ -90,7 +103,45 @@ app.layout = html.Div(
                     [dcc.Graph(id="example-graph-2", figure=fig2)], className="col"
                 ),
         ]),
-        html.Div(output),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        dcc.Graph(id="bar-graph"),
+                    ],
+                    className="col-8",
+                ),
+                html.Div(
+                    [
+                        # dbc.Dropdown(df["BE"].unique(), df["BE"].min(), id="year"),
+                        # dbc.Dropdown(
+                        #     df["emission_type"].unique(),
+                        #     df["emission_type"][0],
+                        #     id="emission_type",
+                        # ),
+                        dbc.Select(
+                            options=[
+                                dict(label=et.strip(), value=et.strip())
+                                for et in df["emission"].unique()
+                            ],
+                            id="emission_type1",
+                            value=df["emission"][0],
+                        ),
+                        dbc.Select(
+                            options=[
+                                dict(label=et.strip(), value=et.strip())
+                                for et in df["emission"].unique()
+                            ],
+                            id="emission_type2",
+                            value=df["emission"][0],
+                        ),
+                    ],
+                    className="col-4",
+                ),
+            ],
+            className="row",
+        ),
+        #html.Div(output),
         html.Div([
             html.Button("Hello", className="btn btn-primary")
         ],className="row",
